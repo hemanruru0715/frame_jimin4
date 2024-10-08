338a38e04529c489478f8322b1d5118f6abc994d
hemanruru0715 <hemanruru@gmail.com>
2024-10-08 20:25:54 +0900

    labels14 수정

--- app/utils/generateChart.ts
+++ app/utils/generateChart.ts
@@ -5,21 +5,21 @@ import { fetchUserDataRecentSevenDaysForChart } from '@/app/utils/supabase';
 // 커스텀 폰트 등록 (TrueType Font 파일 경로)
 registerFont('./public/fonts/Recipekorea.ttf', { family: 'CustomFont' });
 
-// 최근 7일의 날짜 생성 함수
-let last7DaysLabels_MMDD: string[] = [];
-const getLast7DaysLabels = (): string[] => {
+// 최근 14일의 날짜 생성 함수
+let last14DaysLabels_MMDD: string[] = [];
+const getLast14DaysLabels = (): string[] => {
     const today = new Date();
-    const last7DaysLabels = [];
+    const last14DaysLabels = [];
     for (let i = 0; i < 14; i++) {
         const pastDate = new Date(today);
         pastDate.setDate(today.getDate() - i);
         const year = pastDate.getFullYear();
         const month = String(pastDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
         const day = String(pastDate.getDate()).padStart(2, '0');
-        last7DaysLabels.unshift(`${year}-${month}-${day}`);
-        last7DaysLabels_MMDD.unshift(`${month}-${day}`);
+        last14DaysLabels.unshift(`${year}-${month}-${day}`);
+        last14DaysLabels_MMDD.unshift(`${month}-${day}`);
     }
-    return last7DaysLabels;
+    return last14DaysLabels;
 };
 
 // 차트 생성 함수
@@ -37,8 +37,9 @@ export const generateChart = async (fid: any) => { // async 추가
     }
 
     // 최근 7일의 날짜 레이블 생성
-    let labels = getLast7DaysLabels();
+    let labels = getLast14DaysLabels();
 
+    console.log("labels=" + JSON.stringify(labels));
     // sortedUserChartData와 labels를 매칭하여 available_claim_amount 값을 채움
     const availableClaimAmounts = labels.map(label => {
         const entry = sortedUserChartData.find(data => data.record_date_utc.startsWith(label));
@@ -49,7 +50,9 @@ export const generateChart = async (fid: any) => { // async 추가
     const minClaimAmount = Math.min(...availableClaimAmounts); // 최소값
     const maxClaimAmount = Math.max(...availableClaimAmounts); // 최대값
 
-    labels = last7DaysLabels_MMDD; //실제로 x축은 월일만 보여줌
+    let labels14 = last14DaysLabels_MMDD; //실제로 x축은 월일만 보여줌
+
+    console.log("labels14=" + JSON.stringify(labels14));
 
     const canvas = createCanvas(600, 600); // 크기를 더 크게 설정
     const ctx: any = canvas.getContext('2d');
@@ -57,7 +60,7 @@ export const generateChart = async (fid: any) => { // async 추가
     const chartConfig: any = {
         type: 'line',
         data: {
-            labels: labels,
+            labels: labels14,
             datasets: [
                 {
                     //label: 'claim',
